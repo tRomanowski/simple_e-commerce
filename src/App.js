619cbdc8fda14769ca8347cc8de0e4d86b49787b
Tim Romanowski <timromanowski15@gmail.com>
2022-09-09 21:01:30 +0200

    Update to router6 and insert routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+	Home,
+	Products,
+	SingleProduct,
+	About,
+	Cart,
+	Error,
+	Checkout,
+	Private,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+	return (
+		<>
+			<Navbar />
+			<Sidebar />
+			<Routes>
+				<Route path='/' exact element={<Home />} />
+				<Route path='/about' exact element={<About />} />
+				<Route path='/cart' exact element={<Cart />} />
+				<Route path='/products' exact element={<Products />} />
+				<Route path='/products/:id' exact element={<SingleProduct />} />
+				<Route path='/checkout' exact element={<Checkout />} />
+				<Route path='/*' exact element={<Error />} />
+			</Routes>
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
