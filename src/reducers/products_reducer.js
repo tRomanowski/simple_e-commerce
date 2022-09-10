d66ccf5f7f951fd341dfcee5856d76630d9b96d5
Tim Romanowski <timromanowski15@gmail.com>
2022-09-10 13:17:00 +0200

    Fetch products array

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,6 +16,23 @@ const products_reducer = (state, action) => {
 	if (action.type === SIDEBAR_CLOSE) {
 		return { ...state, isSidebarOpen: false };
 	}
+	if (action.type === GET_PRODUCTS_BEGIN) {
+		return { ...state, products_loading: true };
+	}
+	if (action.type === GET_PRODUCTS_SUCCESS) {
+		const featured_products = action.payload.filter(
+			(product) => product.featured === true
+		);
+		return {
+			...state,
+			products: action.payload,
+			products_loading: false,
+			featured_products: featured_products,
+		};
+	}
+	if (action.type === GET_PRODUCTS_ERROR) {
+		return { ...state, products_loading: false, products_error: true };
+	}
 	return state;
 	throw new Error(`No Matching "${action.type}" - action type`);
 };
